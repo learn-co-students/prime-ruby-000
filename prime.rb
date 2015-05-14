513df68d2dcd738d23f461c8a8026a1ebe82e8bf
srz2 <srz222@gmail.com>
2015-05-14 15:08:14 -0400

    Done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(num)
+  
+  if num == 0
+    return false
+  end
+
+  for index in 1..num
+    #puts "Evaluating #{num} % #{index}=" + (num % index).to_s
+    if index == 1 || index == num
+      next
+    end
+    if num % index == 0
+      return false
+    end
+  end
+  return true
+end
