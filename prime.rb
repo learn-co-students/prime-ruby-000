b04baab1d218980469e9e62203f174e73b900d91
Linus Liang <linus.liang@nyu.edu>
2015-05-15 14:14:30 -0400

    prime.rb added with sieve

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
 # Add  code here!
+#really need to run build_sieve once, otherwise not effiecinet
+def build_sieve()
+	isPrime = Array.new(120000)
+	isPrime.fill(true)
+	isPrime[2] = false 
+	for i in (2..60000)
+		ct = 2
+		not_prime = i*ct
+		while (not_prime < 120000)
+			isPrime[not_prime] = false
+			ct+=1
+			not_prime = i*ct
+		end
+	end
+	return isPrime
+end
+
+def prime?(num)
+	isPrime = build_sieve
+	return isPrime[num]
+end
