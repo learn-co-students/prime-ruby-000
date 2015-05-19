507e01fa5de8c1f9a2b439b1a7ae8a40931accef
gjc2118 <gjc2118@gmail.com>
2015-05-18 22:09:48 -0700

    optimized

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
 # Add  code here!
+
+def prime?(int)
+  if int<=1
+    false
+  elsif int<=3
+    true
+  elsif int%2 ==0 || int%3 ==0
+    false
+  else
+    i=5
+    while i*i<=int
+      if int%i ==0 || int%(i+2) ==0
+        false
+        break
+      end
+    i+=6
+    end
+    true
+  end
+end
+    
