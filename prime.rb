762bc8b90fc11acb5170353dbd3b8c3bce033a17
chaplonglau <chaplonglau@gmail.com>
2015-05-27 15:17:53 -0400

    prime

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+
+
+def prime?(num)
+  i=2
+  while (i<num)      
+    if (num%i==0)
+      return false
+    else
+      i+=1
+    end
+  end
+  return true
+end
+
+
+2<4
+4%2==0 
