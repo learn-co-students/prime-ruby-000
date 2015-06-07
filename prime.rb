7b45fad3e970a0e2d9e4ad80fecef86f9bcb719e
Edward <ehyoo97@gmail.com>
2015-06-07 00:19:09 -0400

    done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
+def prime?(number)
+	if number == 2
+		return true
+	elsif number == 3
+		return true
+	elsif number ==7
+		return true
+	elsif number == 11
+		return true
+	elsif number % 2 == 0
+		return false
+	elsif number % 3 == 0
+		return false
+	elsif number %5 == 0
+		return false
+	elsif number %7 ==0
+		return false
+	else
+		return true
+	end
+end
+
 # Add  code here!
