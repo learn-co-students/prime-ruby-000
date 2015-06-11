1a56a783d690ad800037d3e1c4240c12cffb79b9
Rigz777 <rigzgoodmusic@gmail.com>
2015-06-11 13:23:52 -0400

    finally, but this was tough

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(number)
+  if number < 2
+    return false
+  end
+  
+  for index in 1..number
+    if index == 1 || index == number
+      next
+    end
+
+    if number % index == 0
+      return false
+    end
+  end
+  return true
+end
+
