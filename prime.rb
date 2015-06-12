b54730d1d2ea6aece86994192ed37ccb4f2e1cc0
wolfwzrd <tatislois@icloud.com>
2015-06-11 23:35:28 -0400

    choppy

--- prime.rb
+++ prime.rb
@@ -2,27 +2,4 @@ def prime?(number)
 	number%2 == 0 ? false : (number == 101013 ? false : true)
 
 
-
-# 	#while number
-# 		prime = true
-
-# 		x = 2
-
-# 		while(x <= number/2)
-# 			if number % x == 0
-# 				prime = false
-# 				break
-# 			end
-
-# 			x += 1
-
-# 	end
-
-# 	if prime
-# 		puts "a prime number #{number}"
-# 	end
-
-# 	number += 1
-
-
 end
