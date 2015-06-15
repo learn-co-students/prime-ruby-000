1a7ab754573f7e400d0f9dee543218215da516bc
brianduggan <brian.e.duggan@icloud.com>
2015-06-15 19:15:11 -0400

    add prime? method

--- prime.rb
+++ prime.rb
@@ -1,1 +1,20 @@
-# Add  code here!
+
+def prime?(num)
+  factors_array = []  
+  for i in 1..num
+
+    value = num % i
+
+    # puts "#{i} has a remainder of #{value}"
+    
+    if value == 0
+      factors_array.push(i)
+    end
+  end
+  
+  if factors_array.length > 2
+    return false
+  else
+    return true
+  end
+end
