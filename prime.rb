bf185a26d236b18fb1242b02b95d60524f53339d
garettarrowood <garettarrowood@gmail.com>
2015-06-18 11:41:51 -0400

    I feel like I cheated. But how would I have figured this out on my own?

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(number)
+    return number > 1 if number <=3
+    return false if number % 2 == 0 || number % 3 == 0
+ 
+    x = 5
+    while x*x <= number 
+        return false if (number % x == 0 || number % (x + 2) == 0)
+        x += 6
+    end
+    true  
+end
