900c77f6d1135019ad3b7cd27e57dd4fddb6ce13
jonhunter13 <jonhunter13@gmail.com>
2015-07-05 16:07:07 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime? (num)
+  i = 2
+  is_prime = true
+  while i < num
+    if (num % i) == 0
+      is_prime = false
+      break;
+    end
+    i+=1
+  end
+  is_prime
+end
+
+#puts prime?(4)
