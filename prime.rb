509f37f63675c1ce014c519e09a00873f6acc4ae
cornerstonian <vois.cornerstone@gmail.com>
2015-07-06 17:58:23 -0400

    done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+#A prime number (or a prime) is a natural number greater than 1 that has no #positive divisors other than 1 and itself.
+
+def prime?(number)
+  if number < 2
+    return false
+  end
+
+    for index in 1..number
+      if index == 1 || index == number
+        next
+      end
+
+    if number % index == 0
+      return false
+    end
+  end
+    return true
+end
