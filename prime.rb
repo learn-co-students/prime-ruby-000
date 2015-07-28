0ebe63dc56d3e99889d6db41fb8fa874b2bbde64
mendyismyname <seldo6@gmail.com>
2015-07-27 20:37:22 -0400

    done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime? num
+  is_prime = true
+  array = *(2..num-1)
+  array.each do |i|
+    result = num.to_f / i.to_f
+    if result == result.to_i
+      is_prime = false 
+    end
+  end
+  is_prime
+end
