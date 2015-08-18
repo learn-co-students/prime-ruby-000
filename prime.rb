6184e9e9e4728a8c0d97d0992db8afb401f0b91c
NoneOfMaster <rcraigt@gmail.com>
2015-08-18 15:20:17 -0700

    yesgit status

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+require 'pry'
+
+def prime?(arg)
+  answers = []
+    for numbers in 2..(arg - 1) do
+      remainders = arg % numbers
+      answers << remainders
+    end
+  !answers.include?(0)
+end
