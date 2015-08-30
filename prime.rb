0cfdd86543671e4f9624083652a453c37f49278d
allensoberano <allensoberano@gmail.com>
2015-08-30 05:10:01 -0700

    done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
 # Add  code here!
+def prime?(num)
+  range_to_check = (2..num).to_a
+
+  range_to_check.each do |current_number|
+    if num % current_number == 0 && num != current_number
+      return false
+    end
+  end
+
+true
+
+end
