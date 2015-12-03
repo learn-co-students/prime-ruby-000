b37d6229079348a730e20de28c0793de7962315e
alicht <avilichtschein@gmail.com>
2015-12-02 21:58:39 -0500

    completed lab

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime?(num)
+  if num < 2
+    return false
+  end
+
+  (2..num).each do |n|
+    if num % n == 0 && n != num
+      return false
+    end
+  end
+
+  return true
+end
+
+# if num % 2 != 0 || num % 3 != 0 
+#    true
+# else 
+#   false
+# end
