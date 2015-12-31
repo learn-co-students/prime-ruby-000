f727746250cb393d379115874854f9ae26c9bdc4
yancann <jessicayancan@gmail.com>
2015-12-31 17:53:47 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,6 +1,10 @@
+require 'benchmark'
+require 'bigdecimal/math'
 
 def prime?(num)
   return false if num <= 1
   Array(2..Math.sqrt(num)).each {|i| return false if num % i == 0}
   return true
 end
+
+puts Benchmark.measure { BigMath.PI(10_000) }
