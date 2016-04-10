e5f519e5c36dbecc13cb38532eb85ee9f8b9193f
OnlyHughMan <hughvidler@hotmail.com>
2016-04-11 04:19:42 +0800

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+
+
+  
+def prime?(n)
+  res = true
+  for i in 2..n-1
+    if n % i == 0
+      res = false
+    end
+  end
+  if n == 0 
+    res = false
+  end
+   if n == 1 
+    res = false
+  end
+  return res
+end
