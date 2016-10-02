a3713493278c34d67c6b0ddb0bc0fa02a02fafca
pkprosol <peter.prosol@gmail.com>
2016-10-01 22:40:43 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+require 'pry'
+
+def prime?(primeCandidate)
+  isPrime = true
+  if primeCandidate == 0 || primeCandidate == 1
+    isPrime = false
+  else
+    for number in 2...primeCandidate do   
+      if primeCandidate % number == 0 
+        isPrime = false
+      end
+    end
+  end
+  isPrime
+end
