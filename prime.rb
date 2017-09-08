4a3a00f7e0a45a395866f29c383965fddc9bd5ab
Alex Katalkin <alexey.katalkin@gmail.com>
2017-09-08 21:11:06 +0000

    primes

--- prime.rb
+++ prime.rb
@@ -1,1 +1,26 @@
 # Add  code here!
+def prime?(argument)
+
+  dividers = (0..argument).to_a
+  counter = 0
+
+  if argument <=1
+    counter +=1
+  end
+
+  dividers.each do |n|
+    if n != 0 && n != 1 && n != argument && argument % n == 0
+      counter += 1
+    end
+
+  end
+
+  if counter == 0
+
+    true
+  else
+
+    false
+  end
+
+end
